b6b122bf9ced43b37ecc34edc35ca5664e64681f
Koech-code <nixon.koech@moringaschool.com>
2021-08-29 17:56:57 +0300

    crrated a manage file to manage all the files app, including tests and accessing the python shell

--- manage.py
+++ manage.py
@@ -1,13 +1,15 @@
 from app import create_app, db
 from flask_script import Manager, Server
 import app
-# from app.models import User
+from app.models import User
+from  flask_migrate import Migrate, MigrateCommand
 
 app=create_app('development')
 
 manager=Manager(app)
 manager.add_command('server',Server)
-
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.command
 def tests():
@@ -17,6 +19,6 @@ def tests():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db)
+    return dict(app = app,db = db,User=User )
 if __name__ == '__main__':
     manager.run()
