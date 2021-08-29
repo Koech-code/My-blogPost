9641ef64b32be7337d69d23224b5876c16af2096
Koech-code <nixon.koech@moringaschool.com>
2021-08-29 17:40:40 +0300

    imported the mail class from flask-mail and initialized it

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_mail import Mail
 
-db=SQLAlchemy()
-bootstrap=Bootstrap()
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail=Mail()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 def create_app(config_name):
     app=Flask(__name__)
 
@@ -14,10 +22,14 @@ def create_app(config_name):
     # Initializing flask extentions
     db.init_app(app)
     bootstrap.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
 
     # Registering the blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     return app
 
